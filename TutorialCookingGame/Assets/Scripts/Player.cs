1d6f171ec214e53145ca130e0b6314fbadb15e83
Farel Antoine <antoinefarel@protonmail.com>
2023-03-10 21:33:29 +0100

    Player animation

--- TutorialCookingGame/Assets/Scripts/Player.cs
+++ TutorialCookingGame/Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 7f;
+
+    private bool isWalking;
     
     private void Update()
     {
@@ -32,8 +34,15 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
+
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
 
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
